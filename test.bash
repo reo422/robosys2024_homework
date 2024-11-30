7734af933602d9a262bae155d837d252b8552ad8
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2024-11-30 15:34:49 +0900

    ADD test

--- test.bash
+++ test.bash
@@ -14,6 +14,34 @@ res=0
 out=$(echo あいうえお | ./conversion)
 [ "${out}" = "aiueo" ] || ng "$LINENO"
 
+out=$(echo かきくけこ | ./conversion)
+[ "${out}" = "kakikukeko" ] || ng "$LINENO"
+
+out=$(echo さしすせそ | ./conversion)
+[ "${out}" = "sashisuseso" ] || ng "$LINENO"
+
+out=$(echo たちつてと | ./conversion)
+[ "${out}" = "tachitsuteto" ] || ng "$LINENO"
+
+out=$(echo なにぬねの | ./conversion)
+[ "${out}" = "naninuneno" ] || ng "$LINENO"
+
+out=$(echo はひふへほ | ./conversion)
+[ "${out}" = "hahifuheho" ] || ng "$LINENO"
+
+out=$(echo まみむめも | ./conversion)
+[ "${out}" = "mamimumemo" ] || ng "$LINENO"
+
+out=$(echo やゆよ | ./conversion)
+[ "${out}" = "yayuyo" ] || ng "$LINENO"
+
+out=$(echo らりるれろ | ./conversion)
+[ "${out}" = "rarirurero" ] || ng "$LINENO"
+
+out=$(echo わをん | ./conversion)
+[ "${out}" = "wawon" ] || ng "$LINENO"
+
+
 ### 変な動作 ###
 out=$(echo 12345 | ./conversion)
 [ "${out}" = "12345" ] || ng "$LINENO"
