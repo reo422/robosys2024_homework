0b20d6189f8a064a543f577db538c1e2521f0d3e
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2024-12-02 21:36:55 +0900

    ADD test

--- test.bash
+++ test.bash
@@ -72,27 +72,81 @@ out=$(echo ばびぶべぼ | ./conversion)
 out=$(echo ぱぴぷぺぽ | ./conversion)
 [ "${out}" = "papipupepo" ] || ng "$LINENO"
 
+#空文字の確認
+out=$(echo "" | ./conversion)
+[ "${out}" = "" ] || ng "$LINENO"
+
+
 ### 変な動作 ###
 
 #数字が入力されたとき
 out=$(echo 12345 | ./conversion)
-[ "${out}" = "12345" ] || ng "$LINENO"
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
 
-#ひらがなと数字が混じってるとき
-out=$(echo あ123い | ./conversion)
-[ "${out}" = "a123i" ] || ng "$LINENO"
+#英語が入力されたとき
+out=$(echo hello | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
 
-#ひらがなと英語が混じってるとき
-out=$(echo ひらがなandひらがな | ./conversion)
-[ "${out}" = "hiraganaandhiragana" ] || ng "$LINENO"
+#カタカナが入力されたとき
+out=$(echo コンニチハ | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
 
-#空文字があるとき
-out=$(echo "" | ./conversion)
-[ "${out}" = "" ] || ng "$LINENO"
+#記号があるとき
+out=$(echo ./ | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
 
-#空文字とひらがながあるとき
-out=$(echo "おはよう こんにちは" | ./conversion)
-[ "${out}" = "ohayou konnichiha" ] || ng "$LINENO"
+#ひらがなと数字があるとき
+out=$(echo "こんにちは12345" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#ひらがなと英語があるとき
+out=$(echo "こんにちはworld" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#ひらがなとカタカナがあるとき
+out=$(echo "こんにちはコンニチハ" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#ひらがなと記号があるとき
+out=$(echo "こんにちは。" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#数字とひらがながあるとき
+out=$(echo "12345こんにちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#英語とひらがながあるとき
+out=$(echo "worldこんにちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#カタカナとひらがながあるとき
+out=$(echo "コンニチハこんにちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#記号とひらがながあるとき
+out=$(echo "./こんにちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#ひらがなと数字があるとき
+out=$(echo "こんに12345ちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#ひらがなと英語があるとき
+out=$(echo "こんにworldちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#ひらがなとカタカナがあるとき
+out=$(echo "こんにコンニチハちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#ひらがなと記号があるとき
+out=$(echo "こんに，ちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
+#ひらがなと空文字があるとき
+out=$(echo "こんに ちは" | ./conversion)
+[ "${out}" = "エラー: 無効な文字が含まれています．" ] || ng "$LINENO"
+
 
 
 [ "${res}" = 0 ] && echo OK
